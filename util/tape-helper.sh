6135290df0bd507acfbbe85fe2d1085345708982
Justin Wyne <1986068+wyne@users.noreply.github.com>
2024-09-13 02:24:17 -0700

    new tapes

--- util/tape-helper.sh
+++ util/tape-helper.sh
@@ -12,6 +12,7 @@ plugins=(
 
 source $ZSH/oh-my-zsh.sh
 
+## TODO: init fasder with an alternate db file
 alias fasder=~/workspace/fasder/fasder
 
 j() {
@@ -35,5 +36,12 @@ alias z='fasder_cd -d'
 alias a='fasder'
 alias d='fasder -d'
 alias f='fasder -f'
+alias v='fasder -f -e vim'
 
 echo -e "\033[35mThis is purple text\033[0m"
+
+_fasder_preexec() {
+  { eval "fasder --proc $(fasder --sanitize $2)"; } >> /dev/null 2>&1
+}
+autoload -Uz add-zsh-hook
+add-zsh-hook preexec _fasder_preexec
