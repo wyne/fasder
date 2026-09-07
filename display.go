ee5e431fc590d38c84dc3c447f9b935b2aa8be1a
Justin Wyne <1986068+wyne@users.noreply.github.com>
2026-09-06 20:51:48 -0400

    Add rank-only sort flag

--- display.go
+++ display.go
@@ -8,14 +8,26 @@ import (
 // Display
 
 func displaySortedEntries(entries []PathEntry, list bool) {
+	displayEntries(entries, list, false)
+}
+
+func displayRankSortedEntries(entries []PathEntry, list bool) {
+	displayEntries(entries, list, true)
+}
+
+func displayEntries(entries []PathEntry, list bool, rankScore bool) {
 	now := time.Now().Unix()
 	for _, entry := range entries {
 		if list {
 			fmt.Println(entry.Path)
 		} else {
+			score := frecentScore(entry, now)
+			if rankScore {
+				score = entry.Rank
+			}
 			fmt.Printf(
 				"%-11.5f%s\n",
-				frecentScore(entry, now),
+				score,
 				entry.Path)
 		}
 	}
